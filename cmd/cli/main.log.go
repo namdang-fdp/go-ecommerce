0e5f32c36f039e9d2f2f6e8d2b02c96a64fb0935
namdang-fdp <namdangcoder@gmail.com>
2025-10-26 21:16:00 +0700

    feat: log config

---
+++ cmd/cli/main.log.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func main() {
+	// sugar := zap.NewExample().Sugar()
+	// sugar.Infof("Hello %s", "namdang-fdp")
+
+	//2. Create
+	// logger := zap.NewExample()
+	// logger.Info("Hello NewExample")
+	//
+	// // development
+	// logger, _ = zap.NewDevelopment()
+	// logger.Info("Hello NewDevelopment")
+	//
+	// logger, _ = zap.NewProduction()
+	// logger.Info("Hello NewProduction")
+	//
+	//3.
+	encoder := getEncoderLog()
+	sync := getWriterSync()
+	core := zapcore.NewCore(encoder, sync, zapcore.InfoLevel)
+	logger := zap.New(core, zap.AddCaller())
+
+	logger.Info("Info log", zap.Int("line", 1))
+	logger.Error("Error log", zap.Int("line", 2))
+
+}
+
+// format log
+func getEncoderLog() zapcore.Encoder {
+	encodeConfig := zap.NewProductionEncoderConfig()
+	encodeConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encodeConfig.TimeKey = "time"
+	encodeConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encodeConfig.EncodeCaller = zapcore.ShortCallerEncoder
+	return zapcore.NewJSONEncoder(encodeConfig)
+}
+
+func getWriterSync() zapcore.WriteSyncer {
+	file, _ := os.OpenFile("./log/log.txt", os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	syncFile := zapcore.AddSync(file)
+	syncConsole := zapcore.AddSync(os.Stderr)
+	return zapcore.NewMultiWriteSyncer(syncConsole, syncFile)
+}
